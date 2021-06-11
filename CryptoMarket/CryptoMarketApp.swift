cba0b948f5950b4bc995bd81ee2846707b79646f
thetajwar2003 <rahmantajwar131@gmail.com>
2021-06-10 22:10:45 -0400

    Initial Commit

---
+++ CryptoMarket/CryptoMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoMarketApp.swift
+//  CryptoMarket
+//
+//  Created by Tajwar Rahman on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
