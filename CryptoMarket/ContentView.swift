9f8db4c5fe303550ecff68410d19c00d2865e15d
thetajwar2003 <rahmantajwar131@gmail.com>
2021-06-11 16:54:12 -0400

    connected latest listing api

--- CryptoMarket/ContentView.swift
+++ CryptoMarket/ContentView.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     @State var isActive:Bool = false
+    @State var coins: [SingleCoinList] = []
     var body: some View {
         VStack{
             if self.isActive{
-                Text("Hello, world!")
+                List(coins) { coin in
+                    Text(coin.name)
                     .padding()
+                }.onAppear{
+                    API().getListings { (coin) in
+                        self.coins = coin.data
+                    }
+                }
             } else {
                 ZStack{
                     Color(UIColor(red: 0/255, green: 72/255, blue: 203/255, alpha: 1)).edgesIgnoringSafeArea(.all)
